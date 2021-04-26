9709334abedaa45a944b63d7a282d32476f127ce
ttran31 <ttran31@vt.edu>
2021-04-26 18:35:00 -0400

    added submenu 9 procedures implementations

--- proj3.sh
+++ proj3.sh
@@ -1,4 +1,199 @@
 #!/bin/bash
+function Op1 {
+	# print header for submenu
+	printf -- '-%.0s' {1..70}; echo ""
+	echo System information
+	printf -- '-%.0s' {1..70}; echo ""
+	
+	# print information for submenu
+	echo Operating System: "$(uname)"
+	lsb_release -a
+	
+	# stay on submenu until user presses enter alone
+	while :
+	do
+		read -n1 -s -r -p "Press [Enter] key to continue ..." key
+		if [ "$key" = '' ]; then
+			printf "\n"
+			break
+		fi
+		printf "\n"
+	done
+}
+
+function Op2 {
+	# print header for submenu
+	printf -- '-%.0s' {1..70}; echo ""
+	echo Hostname and DNS information
+	printf -- '-%.0s' {1..70}; echo ""
+
+	# print submenu information
+	echo Hostname : "$HOSTNAME"
+	echo DNS domain : "$(dnsdomainname)"
+	fqdns=$(nslookup $(hostname) | sed -n s/Name:.//p)
+	fqdn="$(echo "$fqdns" | cut -d$'\n' -f1)"
+	echo Fully qualified domain name : "$fqdn"
+	# get IP address to print
+	info=$(hostname -I)	
+	ip="$(echo "$info" | cut -d' ' -f2)"
+	echo 'Network address (IP) : ' " $ip"
+	servers=$(grep "nameserver" /etc/resolv.conf)
+	printf "DNS name servers (DNS IP) : "
+       	echo "$servers" | cut -d' ' -f2 
+
+	# stay on submenu until user presses enter alone
+	while :
+	do
+		read -n1 -s -r -p "Press [Enter] key to continue ..." key
+		if [ "$key" = '' ]; then
+			printf "\n"
+			break
+		fi
+		printf "\n"
+	done
+}
+
+function Op3 {
+	# print header for submenu
+	printf -- '-%.0s' {1..70}; echo ""
+	echo Network information
+	printf -- '-%.0s' {1..70}; echo ""
+
+	# print submenu information
+	inter=$(ip -o link show | wc -l)
+	echo Total network interfaces found : "$inter"
+	echo '*** IP Addresses Information ***'
+	ip addr show
+	printf '*%.0s' {1..23}; echo ""
+	echo '*** Network routing ***'
+	printf '*%.0s' {1..23}; echo ""
+	route
+	printf '*%.0s' {1..37}; echo ""
+	echo '*** Interface traffic information ***'
+	printf '*%.0s' {1..37}; echo ""
+	netstat -i	
+	# stay on submenu until user presses enter alone
+	while :
+	do
+		read -n1 -s -r -p "Press [Enter] key to continue ..." key
+		if [ "$key" = '' ]; then
+			printf "\n"
+			break
+		fi
+		printf "\n"
+	done
+}
+function Op4 {
+	# print header for submenu
+	printf -- '-%.0s' {1..70}; echo ""
+	echo Who is online
+	printf -- '-%.0s' {1..70}; echo ""
+
+	# print submenu information
+	printf "NAME\tLINE\t\tTIME\t\tCOMMENT\n"
+	who
+
+	# stay on submenu until user presses enter alone
+	while :
+	do
+		read -n1 -s -r -p "Press [Enter] key to continue ..." key
+		if [ "$key" = '' ]; then
+			printf "\n"
+			break
+		fi
+		printf "\n"
+	done
+}
+
+function Op5 {
+	# print header for submenu
+	printf -- '-%.0s' {1..70}; echo ""
+	echo List of last logged in users
+	printf -- '-%.0s' {1..70}; echo ""
+
+	# print submenu information
+	last
+
+	# stay on submenu until user presses enter alone
+	while :
+	do
+		read -n1 -s -r -p "Press [Enter] key to continue ..." key
+		if [ "$key" = '' ]; then
+			printf "\n"
+			break
+		fi
+		printf "\n"
+	done
+}
+function Op6 {
+	# print header for submenu
+	printf -- '-%.0s' {1..70}; echo ""
+	echo Public IP information
+	printf -- '-%.0s' {1..70}; echo ""
+
+	# print submenu information
+	host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}'
+	# stay on submenu until user presses enter alone
+	while :
+	do
+		read -n1 -s -r -p "Press [Enter] key to continue ..." key
+		if [ "$key" = '' ]; then
+			printf "\n"
+			break
+		fi
+		printf "\n"
+	done
+}
+
+function Op7 {
+	# print header for submenu
+	printf -- '-%.0s' {1..70}; echo ""
+	echo Disk Usage Info
+	printf -- '-%.0s' {1..70}; echo ""
+
+	# print submenu information
+	
+	# stay on submenu until user presses enter alone
+	while :
+	do
+		read -n1 -s -r -p "Press [Enter] key to continue ..." key
+		if [ "$key" = '' ]; then
+			printf "\n"
+			break
+		fi
+		printf "\n"
+	done
+}
+
+function Op9 {
+	while : 
+	do
+		echo "(please enter the number of your selection below)"
+		echo 1. Show all proceses
+		echo 2. Kill a process
+		echo 3. Bring up top
+		echo 4. Return to Main Menu
+		echo ""
+
+		read key
+
+		case $key in
+			1)
+				ps -ef				
+			;;
+			2)
+				read -p "Please enter the PID of the process you would like to kil:\n" proc
+				kill $proc
+			;;
+			3)
+				top
+			;;
+			4)
+				break
+		esac
+	done
+
+}
 while :
 do
 	echo $(date)
@@ -16,10 +211,40 @@ do
 	echo 9. Process operations
 	echo 10. Exit
 	read -p "Enter your choice [1 - 10] " choice
-	stop="10"
-	echo $choice
-	if (( $choice == 10 )) ;
-	then
-		break
-	fi
+	
+	case $choice in 
+		1)
+			Op1
+		;;
+		2)
+			Op2
+		;;
+		3)
+			Op3
+		;;
+		4)
+			Op4
+		;;
+		5)
+			Op5
+		;;
+		6)
+			Op6
+		;;
+		7)
+			Op7
+		;;
+		8)
+			bash proj1.sh ~ filetree.html
+		;;
+		9)
+			Op9
+		;;
+		10)
+			break
+		;;
+		*)
+			continue
+
+	esac
 done
